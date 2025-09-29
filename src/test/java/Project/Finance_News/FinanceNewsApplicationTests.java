4b307ccc0660370ed44be30f91e4122dbda2e8dd
ParkSeYoung <tpdudtpdud128@gmail.com>
2025-09-29 17:15:36 +0900

    no message

--- src/test/java/Project/Finance_News/FinanceNewsApplicationTests.java
+++ src/test/java/Project/Finance_News/FinanceNewsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FinanceNewsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
