f31492a2945be8347fff1ab656212f65995f7428
kim-jun-won <junwon2631@gmail.com>
2025-11-01 15:11:03 +0900

    홈화면 수정 + 마이페이지 추가

--- src/main/java/Project/Finance_News/controller/HomeController.java
+++ src/main/java/Project/Finance_News/controller/HomeController.java
@@ -2,17 +2,19 @@ package Project.Finance_News.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.ui.Model;
 import Project.Finance_News.domain.User;
 import Project.Finance_News.domain.session.SessionConst;
+import Project.Finance_News.domain.login.loginForm;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(HttpServletRequest request, Model model) {
+    public String home(HttpServletRequest request, Model model, @ModelAttribute("loginForm") loginForm loginForm) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             User loginUser = (User) session.getAttribute(SessionConst.LOGIN_USER);
