268033a1cac527c01ede4aa6f7fa931b715979b3
kim-jun-won <junwon2631@gmail.com>
2025-11-11 13:22:49 +0900

    test용 db H2메모리 설정(CI/CD)

--- backend/src/test/java/Project/Finance_News/FinanceNewsApplicationTests.java
+++ backend/src/test/java/Project/Finance_News/FinanceNewsApplicationTests.java
@@ -2,8 +2,10 @@ package Project.Finance_News;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinanceNewsApplicationTests {
 
     @Test
